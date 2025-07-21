2c1000775aa0da9ac808376eac2ea3c679923155
Patel Swapnil <info.tapovan2@gmail.com>
2025-07-21 17:32:52 +0530

    first

---
+++ app/provider.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { GoldRatesProvider } from "@/context/GoldRatesContext";
+import { Toaster } from "@/components/ui/toaster";
+import { ChatBot } from "@/components/chatbot";
+
+export function AppProviders({ children }: { children: React.ReactNode }) {
+  return (
+    <GoldRatesProvider>
+      {children}
+      <ChatBot />
+      <Toaster />
+    </GoldRatesProvider>
+  );
+}
