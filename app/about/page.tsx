14ee3af45ac27773f84e85b26a9ec3a78f7f56f0
swapnil1530 <patelswapnil1004@gmail.com>
2025-09-06 15:52:35 +0530

    fix seo

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,6 +5,9 @@ export const metadata: Metadata = {
   title: "About | Balkrushna",
   description:
     "Learn more about Balkrushna Jewellers, our heritage, craftsmanship, and commitment to excellence in creating exquisite jewelry pieces.",
+  alternates:{
+    canonical:"https://balkrushnajewellers.com/about"
+  }
 };
 
 export default function AboutPage() {
--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -4,6 +4,9 @@ import CatalogPage from "./Catalog";
 export const metadata: Metadata = {
   title: "Catalog | Balkrushna",
   description: "Explore Our Catalog Page",
+  alternates: {
+    canonical: "https://balkrushnajewellers.com/catalog",
+  },
 };
 
 export default function Catalog() {
