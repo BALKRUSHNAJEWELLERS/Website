8d315c9e45dd72a726bf4f94a16d6ce174b39202
swapnil1530 <patelswapnil1004@gmail.com>
2025-09-26 08:48:08 +0530

    fix:canonical

--- app/ring-measurement/page.tsx
+++ app/ring-measurement/page.tsx
@@ -5,7 +5,9 @@ export const metadata: Metadata = {
   title: "Ring Measurement | Balkrushna",
   description:
     "Find your perfect ring size with our easy-to-use Ring Measurement Guide. Ensure a comfortable fit for your special moments with Balkrushna Jewellers.",
-  
+  alternates:{
+    canonical: "https://balkrushnajewellers.com/ring-measurement",
+  }
 };
 
 export default function Ring() {
